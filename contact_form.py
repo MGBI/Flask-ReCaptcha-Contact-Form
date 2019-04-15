86df013654a1d531de3ce832d8731a96e0a00d40
Mateusz <grabekm90@users.noreply.github.com>
2019-04-15 07:33:12 +0200

    Adapt to reCAPTCHA v3. Use RECAPTCHA_SITE_KEY and RECAPTCHA_SECRET_KEY env variables

--- contact_form.py
+++ contact_form.py
@@ -1,18 +1,32 @@
-""" 
+"""
 Contact form : Implementing ReCaptcha with Flask.
 
 Read how it works at : http://techmonger.github.io/5/python-flask-recaptcha/
 """
 
-from flask import Flask, request, redirect, render_template, url_for, flash, get_flashed_messages
-import requests, json
-
+import os
+from flask import Flask, request, redirect, render_template, url_for, flash
+import json
+import requests
 
 app = Flask(__name__)
 
 # Secret for message flashing
 
 app.secret_key = 'change-this'
+
+
+def get_env_setting(setting, default=None, obligatory=True):
+    """Get the environment setting or return an exception"""
+    var = os.getenv(setting, default)
+    if not var and obligatory:
+        raise RuntimeError('Set the {} env variable'.format(setting))
+    return var
+
+
+# reCAPTCHA keys
+sitekey = get_env_setting("RECAPTCHA_SITE_KEY")
+secret = get_env_setting("RECAPTCHA_SECRET_KEY")
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -24,38 +38,36 @@ def home():
 @app.route("/contact/", methods=["GET", "POST"])
 def contact():
     """ Renders contact form on get and processes it on post. """
-    
-    sitekey = "your-site-key-here"
 
     if request.method == "POST":
-        name = request.form['name']
-        email = request.form['email']
-        msg = request.form['message']
-        captcha_response = request.form['g-recaptcha-response']
-        
+        request.form['name']
+        request.form['email']
+        request.form['message']
+        captcha_response = request.form['captcha-response']
+
         if is_human(captcha_response):
             # Process request here
-            status = "Detail submitted successfully."
+            status = "Details submitted successfully."
         else:
-             # Log invalid attempts
-            status = "Sorry ! Bots are not allowed."
+            # Log invalid attempts
+            status = "Sorry! Bots are not allowed."
 
         flash(status)
         return redirect(url_for('contact'))
 
     return render_template("contact.html", sitekey=sitekey)
-                
 
-                
+
 def is_human(captcha_response):
     """ Validating recaptcha response from google server.
-        Returns True captcha test passed for the submitted form 
+        Returns True captcha test passed for the submitted form
         else returns False.
     """
-    secret = "your-secret-key-here"
-    payload = {'response':captcha_response, 'secret':secret}
-    response = requests.post("https://www.google.com/recaptcha/api/siteverify", payload)
+    payload = {'response': captcha_response, 'secret': secret}
+    response = requests.post(
+        "https://www.google.com/recaptcha/api/siteverify", payload)
     response_text = json.loads(response.text)
+    flash(response_text)
     return response_text['success']
 
 
